ae394253d13fec2b7fc0e535ea426a9727e02e9c
wayu <162482249+awuio@users.noreply.github.com>
2025-11-16 17:25:40 +0700

    product crud

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,43 +9,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form method="POST" action="{{ route('products.update', $product) }}">
+                    <form method="POST" action="{{ route('products.store') }}">
                         @csrf
-                        @method('PUT')
 
                         <div>
                             <div>
                                 <label for="name">Name:</label>
                             </div>
-                            <input type="text" name="name" id="name" value="{{ $product->name }}"
+                            <input type="text" name="name" id="name"
                                 class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                         </div>
-
                         <div>
                             <div>
                                 <label for="description">Description:</label>
                             </div>
                             <textarea type="text" name="description" id="description"
-                                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">{{ $product->description }}</textarea>
+                                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"></textarea>
                         </div>
-
                         <div>
                             <div>
                                 <label for="price">Price:</label>
                             </div>
-                            <input type="text" name="price" id="price" value="{{ $product->price }}"
+                            <input type="text" name="price" id="price"
                                 class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                         </div>
                         <div>
-
                         <div>
                             <div>
                                 <label for="quantity">Quantity:</label>
                             </div>
-                            <input type="text" name="quantity" id="quantity" value="{{ $product->quantity }}"
+                            <input type="text" name="quantity" id="quantity"
                                 class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                         </div>
-
                         <div>
                             <div>
                                 <label for="category_id">Category:</label>
@@ -53,11 +48,10 @@
                             <select name="category_id" id="category_id"
                                 class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                 @foreach ($categories as $category)
-                                    <option value="{{ $category->id }}" @selected($category->id == $product->category_id)>{{ $category->name }}</option>
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
                                 @endforeach
                             </select>
                         </div>
-
                         <div>
                             <button type="submit"
                                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,30 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <a href="{{ route('products.create')}}">Add new product</a>
-                    <table> 
+                    <a href="{{ route('products.create') }}">Add new product</a>
+                    <table>
                         <thead>
                             <tr>
                                 <th>Name</th>
                                 <th>Category</th>
-                                <th>Price</th>
-                                <th>Quantity</th>
                                 <th>Action</th>
+
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($products as $product)
+                            @foreach ($products as $product)
                                 <tr>
                                     <td>{{ $product->name }}</td>
-                                    <td>{{ $product->category }}</td>
-                                    <td>{{ $product->price }}</td>
-                                    <td class="border">{{ $product->quantity }}</td>
+                                    <td>{{ $product->category->name }}</td>
                                     <td>
                                         <a href="{{ route('products.edit', $product) }}">Edit</a>
                                         <form action="{{ route('products.destroy', $product) }}" method="post">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
+                                            <button type="submit"
+                                                onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
                                         </form>
                                     </td>
                                 </tr>
